8de8d100bfd51d0115a843cc241dc3cffc7b3516
Corey Carter <corey-carter@hotmail.com>
2020-02-20 16:52:03 -0700

    Added TTTBoard class

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,19 @@
 import 'package:tic_tac_toe/tic_tac_toe.dart' as tic_tac_toe;
 
-void main(List<String> arguments) {
-  print('Hello world: ${tic_tac_toe.calculate()}!');
+import 'package:tic_tac_toe/ttt_board.dart';
+
+void main() {
+  TTTBoard board = TTTBoard();
+  print(board);
+
+  board.move(CellType.X, 2);
+  print(board);
+
+  board.move(CellType.X, 4);
+  print(board);
+
+  board.move(CellType.X, 6);
+  print(board);
+
+  print("Winner: ${board.getWinner()}");
 }
--- test/tic_tac_toe_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:tic_tac_toe/tic_tac_toe.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
